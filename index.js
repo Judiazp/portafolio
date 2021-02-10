14b8b6ebb254125111f229a3c90acfd5bf9e4e3e
judiazp <judiazp2p@gmail.com>
2021-02-10 19:04:01 -0300

    Agregando comportamientos a la seccion de bienvenida

--- index.js
+++ index.js
@@ -1,16 +1,25 @@
+const miNombre = document.getElementById("name");
+const navbar = document.querySelector(".header-a");
+
 let nombre = "Juan Carlos y soy Frontend Developer.";
 let i = 0;
 
 const typing = () => {
-
+    
      if ( i < nombre.length) {
     
-        document.getElementById("name").innerHTML += nombre.charAt(i);
+        miNombre.innerHTML += nombre.charAt(i);
         i++;
         setTimeout(typing, 100);
-           
+        
     }
-}
 
+}
 typing();
+
+navbar.addEventListener('click', () => {
+    miNombre.innerText = '';
+    i = 0;
+    typing();
+} );
 
