9c2a76d5a90ee2b53540093d7802489083e5fb24
Juan Carlos Diaz <judiazp2p@gmail.com>
2021-05-28 18:24:26 -0300

    corrigiendo rutas de la aplicacion

--- src/App.js
+++ src/App.js
@@ -25,12 +25,12 @@ function App() {
     <ThemeProvider theme={themeConfig}>
       <Router>
         <Switch>
-          { (loading) ? <Loading/> :  <Route exact path = '/'component={ Welcome } /> } 
+          { (loading) ? <Loading/> :  <Route exact path = 'Judiazp/portafolio/'component={ Welcome } /> } 
           { (loading) ? <Loading/> :
-            <Route exact path= '/portfolio'>
+            <Route exact path= 'Judiazp/portafolio/projects'>
               <Project projectList = { projectList } />
             </Route> }
-          { (loading) ? <Loading/> : <Route exact path = '/contact' component={Contact}/> } 
+          { (loading) ? <Loading/> : <Route exact path = 'Judiazp/portafolio/contact' component={Contact}/> } 
           { (loading) ? <Loading/> : <Route component={PageNotFound}/> }
         </Switch>
       </Router>
