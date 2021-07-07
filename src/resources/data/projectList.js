bfb3600cc248ebbbe76e0cc067c5df3e02ebb981
Juan Carlos Diaz <judiazp2p@gmail.com>
2021-07-07 18:18:01 -0300

    Actualizando descripción del proyecto Finance

--- src/resources/data/projectList.js
+++ src/resources/data/projectList.js
@@ -6,31 +6,40 @@ import develop from '../multimedia/enDesarrollo.jpg'
 
 const projectList = [
     {
+        linkPage: 'https://finance-develop.netlify.app/',
+        linkCode: 'https://github.com/Judiazp/administration-app',
+        img: `${finance}`,
+        title: 'Finance (En desarrollo)',
+        text: 'Aplicación web para administrar finanzas personales. Agrega egresos e ingresos, lleva un registro de tus gastos.',
+        isPublic: true,
+        id: '2'
+    },
+    {
         linkPage: 'https://judiazp.github.io/gif-expert-app/',
         linkCode: 'https://github.com/Judiazp/gif-expert-app',
         img: `${develop}`,
         title: 'GifExpertApp',
-        text: 'App para la búsqueda de gif, construida con React.Js, consumiendo la Api de Giphy.',
+        text: 'App para la búsqueda de gif, construida con React.Js, consumiendo la API de GIPHY.',
         isPublic: true,
         id: '1'
-    },
-    {
-        linkCode: 'https://github.com/Judiazp/portafolio',
-        img: `${portfolio}`,
-        title: 'Portafolio',
-        text: 'Construido con ReactJS utilizando componentes de Material-UI.',
-        isPublic: false,
-        NoInterface: true,
-        id: '4'
     },
     {
         linkPage: 'https://judiazp.github.io/app-lista-mercado/',
         linkCode: 'https://github.com/Judiazp/app-lista-mercado',
         img: `${todo}`,
         title: 'Todo List',
-        text: 'Aplicación para listas de tareas. Filtra por completadas, no completadas, y mas.',
+        text: 'Crea listas de tareas. Filtra por completadas, no completadas, y mas. Construida con HTML5, CSS3 y vanilla JavaScript.',
         isPublic: true,
         id: '3'
+    },
+    {
+        linkCode: 'https://github.com/Judiazp/portafolio',
+        img: `${portfolio}`,
+        title: 'Portafolio',
+        text: 'Construido con React.JS utilizando componentes de Material-UI.',
+        isPublic: false,
+        NoInterface: true,
+        id: '4'
     },
     {
         linkPage: 'https://judiazp.github.io/documentation-JavaScript/',
@@ -40,15 +49,6 @@ const projectList = [
         text: 'Página de documentación técnica de JavaScript construida con HTML5 y CSS3.',
         isPublic: true,
         id: '5'
-    },
-    {
-        linkPage: 'https://finance-develop.netlify.app/',
-        linkCode: 'https://github.com/Judiazp/administration-app',
-        img: `${finance}`,
-        title: 'Finance (En desarrollo)',
-        text: 'La plataforma se encuentra en desarrollo y pruebas por lo que recomiendo visitar la app junto al repositorio.',
-        isPublic: true,
-        id: '2'
     },
 ];
 
